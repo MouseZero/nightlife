ce4f5bf8b1e501980d10ac69f8ffb558bb994436
mousezero <rm@mousezero.com>
2017-02-27 20:37:34 -0800

    api does serve up a page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const PORT = process.argv[2] || 3333
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.json({
+    success: true,
+    msg: 'it worked'
+  })
+})
+
+app.listen(PORT, function () {
+  console.log(`App is listening on port ${PORT}`)
+})
