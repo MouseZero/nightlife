c0cdfa4624697f82d80439e8d16b842af6d58897
mousezero <rm@mousezero.com>
2017-03-07 21:11:44 -0800

    (refac)  fixed code warnings

--- src/persistence/query.js
+++ src/persistence/query.js
@@ -1,21 +1,20 @@
 const _ = require('lodash')
 
-module.exports = _.curry( function(pool, queryString){
-  const params = arguments[2] || [];
-  return new Promise(function(resolve, reject){
-
-    pool.connect( function(err, client, done){
-      if(err){
-        return reject('Error fetching client pool for postresql');
+module.exports = _.curry(function (pool, queryString) {
+  const params = arguments[2] || []
+  return new Promise(function (resolve, reject) {
+    pool.connect(function (err, client, done) {
+      if (err) {
+        return reject(err)
       }
 
-      client.query(queryString, params, function(err, result){
-        if(err){
-          return reject('error running query:: ' + err);
+      client.query(queryString, params, function (err, result) {
+        if (err) {
+          return reject(err)
         }
-        resolve(result.rows);
-        done();
-      });
-    });
-  });
-});
+        resolve(result.rows)
+        done()
+      })
+    })
+  })
+})
