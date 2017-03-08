a2602f317b03803479471099f555f016c11fd13c
mousezero <rm@mousezero.com>
2017-03-07 17:07:32 -0800

    (feat) create user table

--- tools/database/setupTables.js
+++ tools/database/setupTables.js
@@ -3,9 +3,24 @@ const pg = require('pg')
 const pool = new pg.Pool(config)
 const query = require('../../src/persistence/query')(pool)
 
-query(`SELECT * FROM users`)
+query(`
+  CREATE TABLE public.users
+  (
+    id SERIAL,
+    name character(25),
+    password character(300),
+    CONSTRAINT id PRIMARY KEY (id),
+    CONSTRAINT name UNIQUE (name)
+  )
+  WITH (
+    OIDS=FALSE
+  );
+  ALTER TABLE public.users
+    OWNER TO dev;
+`)
 .then(function(x) {
-  console.log(x);
+  console.log('Created users table')
+  process.exit()
 })
 .catch(function (err) {
   console.log(err)
