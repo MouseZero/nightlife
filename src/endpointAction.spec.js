08082b708b959f43eb93a35f1b36288b606aa906
mousezero <rm@mousezero.com>
2017-03-10 18:40:23 -0800

    (refac)  converted testing promise consumption to async/await

--- src/endpointAction.spec.js
+++ src/endpointAction.spec.js
@@ -9,32 +9,18 @@ const TESTPASS = 'password'
 
 describe('Should be able to control user opperations', function () {
   describe('createUserInject', function () {
-    it('should retern success if makeUser is successful', function (done) {
+    it('should retern success if makeUser is successful', async function () {
       const makeUser = () => Promise.resolve(true)
-      createUserInject(TESTUSER, TESTPASS, { makeUser })
-      .then(result => {
-        if (result.success) {
-          done()
-        } else {
-          done('should have been a success')
-        }
-      })
+      const result = await createUserInject(TESTUSER, TESTPASS, { makeUser })
+      if (result.success) return Promise.resolve()
+      return Promise.reject(new Error('should have been a success'))
     })
 
-    it('should return err if makeUser errors', function (done) {
-      const makeUser = () => {
-        return Promise.reject(new Error('Was not able to create user'))
-      }
-
-      createUserInject(TESTUSER, TESTPASS, { makeUser })
-      .then(function (result) {
-        if (isNotSuccess(result)) {
-          done()
-        } else {
-          done('could not find {success: false}')
-        }
-      })
-      .catch(() => done('Should not have throwen an error'))
+    it('should return err if makeUser errors', async function () {
+      const makeUser = () => Promise.reject(new Error())
+      const result = await createUserInject(TESTUSER, TESTPASS, { makeUser })
+      if (isNotSuccess(result)) return Promise.resolve()
+      return Promise.reject(new Error('Should have returned "success: false"'))
     })
   })
 
