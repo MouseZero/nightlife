6cd183613146852db28fcf8966ffb7b0538a6449
mousezero <rm@mousezero.com>
2017-03-10 19:31:51 -0800

    (feat) can parse incoming query

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const PORT = process.argv[2] || 3333
 const app = require('express')()
 const routes = require('./src/routes')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/', routes())
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
-const endPointAction = require('./endPointAction')
+const endPointAction = require('./endpointAction')
 
 module.exports = function routes () {
   router.get('/', function (req, res) {
     res.json(endPointAction.testEndPoint())
+  })
+
+  router.post('/test', function (req, res) {
+    res.json({
+      success: true,
+      name: req.body.name,
+      password: req.body.password
+    })
   })
 
   router.get('/authenticate', function (req, res) {
