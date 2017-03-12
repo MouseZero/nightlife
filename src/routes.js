40ad4ad18826cde5100fb119547f03aeb9507644
mousezero <rm@mousezero.com>
2017-03-11 16:48:07 -0800

    (feat) user can get to a jwt proteced endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const endPointAction = require('./endpointAction')
 const pg = require('pg')
-const { database: config } = require('../config.json')
+const { database: config, secret } = require('../config.json')
 const pool = new pg.Pool(config)
 const usersDb = require('./persistence/usersFactory')(pool)
+const jwtPromise = require('./util/jwtPromise')
 
 module.exports = function routes () {
   router.get('/', function (req, res) {
@@ -14,6 +15,25 @@ module.exports = function routes () {
     const name = req.body.name
     const password = req.body.password
     res.json(await endPointAction.authenticate([name, password], usersDb))
+  })
+
+  router.use(function (req, res, next) {
+    let token = req.body.token || req.query.token || req.headers['x-access-token']
+    jwtPromise.verify(token, secret)
+    .then(function (decoded) {
+      req.decoded = decoded
+      next()
+    })
+    .catch(function (err) {
+      res.json({success: false, message: err})
+    })
+  })
+
+  router.get('/hide', function (req, res) {
+    res.json({
+      success: false,
+      msg: 'you got here'
+    })
   })
 
   return router
