6b990f84b050a2476c28bcc8a3d65885b53f5073
MouseZero <rm@mousezero.com>
2017-03-24 19:35:57 -0700

    work done on the api

--- src/util/yelpApiInterface.js
+++ src/util/yelpApiInterface.js
@@ -1,5 +1,13 @@
+const { yelp: { app_id, app_secret } } = require('../../config')
+const fetch = require('node-fetch')
+const { xWwwFormUrlencodedFetch } = require('./requestEndpoints')(fetch)
+
 async function search (searchFor) {
-  return Promise.resolve({})
+  const body = {
+    'client_id': app_id,
+    'client_secret': app_secret
+  }
+  return await xWwwFormUrlencodedFetch(body, 'https://api.yelp.com/oauth2/token')
 }
 
 module.exports = {
--- src/util/yelpApiInterface.spec.js
+++
@@ -1,9 +0,0 @@
-const { search } = require('./yelpApiInterface')
-const { expect } = require('chai')
-
-describe('yelp api interface', function () {
-  it('should return json object for successful call', async function () {
-    const result = await search('irvine')
-    expect(result).to.be.a('object')
-  })
-})
