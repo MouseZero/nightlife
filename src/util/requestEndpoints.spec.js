780ff2a8d3b1f22d1aa97dd5ed5e87add2cb22d2
MouseZero <rm@mousezero.com>
2017-03-24 20:01:31 -0700

    (test)  added non happy path test for fetch wrapper

--- src/util/requestEndpoints.spec.js
+++ src/util/requestEndpoints.spec.js
@@ -28,5 +28,24 @@ describe('requestEndpoints', () => {
         expect(result).to.equal('success')
       })
     })
+
+    context('unsuccessful response', async () => {
+      let fetch
+      beforeEach(() => {
+        fetch = stub(t, 'fetch').callsFake((x) => {
+          return Promise.reject(new Error('err'))
+        })
+      })
+      afterEach(() => {
+        fetch.restore()
+      })
+
+      it('throws error', (done) => {
+        requestEndpoints(fetch)
+          .xWwwFormUrlencodedFetch()
+          .then(() => done('should have errored'))
+          .catch(() => done())
+      })
+    })
   })
 })
