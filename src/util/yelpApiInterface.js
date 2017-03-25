d4af5afbb4433b962284acb322e4470a218b0c56
MouseZero <rm@mousezero.com>
2017-03-24 22:29:08 -0700

    (refac)  Parameterize dependencies for testing

--- src/util/yelpApiInterface.js
+++ src/util/yelpApiInterface.js
@@ -2,14 +2,20 @@ const { yelp: { app_id, app_secret } } = require('../../config')
 const fetch = require('node-fetch')
 const { xWwwFormUrlencodedFetch } = require('./requestEndpoints')(fetch)
 
-async function search (searchFor) {
+const getToken = (endpointInterface) => async () => {
   const body = {
     'client_id': app_id,
     'client_secret': app_secret
   }
-  return await xWwwFormUrlencodedFetch(body, 'https://api.yelp.com/oauth2/token')
+  return await endpointInterface(body, 'https://api.yelp.com/oauth2/token')
 }
 
-module.exports = {
-  search
+module.exports = () => {
+  return {
+    getToken: getToken(xWwwFormUrlencodedFetch)
+  }
 }
+
+Object.assign(module.exports, {
+  getToken
+})
