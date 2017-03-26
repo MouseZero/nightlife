127ae1bb1c700255df4db8888d2691cad6a3705d
MouseZero <rm@mousezero.com>
2017-03-26 14:30:29 -0700

    (test)  Added first integration test

--- src/util/yelpApiInterface.integ.js
+++ src/util/yelpApiInterface.integ.js
@@ -1,5 +1,13 @@
 const { getToken } = require('./yelpApiInterface')()
+const { expect } = require('chai')
 
-
-getToken()
-  .then(x => console.log(x))
+describe('yelpApiInterface', () => {
+  describe('getToken', () => {
+    it('Should return the right keys', async () => {
+      const result = await getToken()
+      console.log(result)
+      expect(result).to.include.keys('expires_in', 'access_token', 'token_type')
+      expect(result['token_type']).to.equal('Bearer')
+    })
+  })
+})
