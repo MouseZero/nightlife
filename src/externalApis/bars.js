d4c367ca3f3753cbf6c8846e8424438086ce5893
MouseZero <rm@mousezero.com>
2017-03-28 21:54:21 -0700

    moving state out of the tested function

--- src/externalApis/bars.js
+++ src/externalApis/bars.js
@@ -2,16 +2,20 @@ const yelp = require('./yelpApiInterface')()
 const tokenFetcher = require('./tokenFetcher')(yelp.getToken)
 let token
 
-const search = (yelp) => async (location) => {
+const updateToken = async () => {
+  return await tokenFetcher.updateToken(yelp.getToken)
+}
+
+const search = (updateToken, yelp) => async (location) => {
   if (!token) {
-    token = await tokenFetcher.updateToken(yelp.getToken)
+    token = await updateToken()
   }
   return await yelp.searchBars(location, token.token)
 }
 
 module.exports = () => {
   return {
-    search: search(yelp)
+    search: search(updateToken, yelp)
   }
 }
 Object.assign(module.exports, {
