ea04b09d6c82b3226c47bc5a814cbd251d3257c1
MouseZero <rm@mousezero.com>
2017-04-01 21:44:41 -0700

    (refac)  JWT middleware verification unit is in place

--- src/jwt-middleware.js
+++ src/jwt-middleware.js
@@ -4,19 +4,6 @@ const { sign } = require('jsonwebtoken')
 const { secret } = require('../config.json')
 const { BadRequest } = require('./custom-errors')
 
-// Middleware that requires a token for the rest of the API end points
-const jwt = (verify, secret) => wrap(async (req, res, next) => {
-  let token = req.body.token || req.query.token || req.headers['x-access-token']
-  verify(token, secret)
-  .then(decoded => {
-    req.decoded = decoded
-    next()
-  })
-  .catch(err => {
-    res.json({success: false, message: err})
-  })
-})
-
 const authenticate = (getUser, secret, sign) => wrap(async (req, res, next) => {
   if (!req.body.user || !req.body.password) {
     throw new Error('User name and password is required')
@@ -42,13 +29,12 @@ const mustHaveJWT = (verify, secret) => wrap(async (req, res, next) => {
 
 module.exports = (users) => {
   return {
-    jwt: jwt(verify, secret),
+    mustHaveJWT: mustHaveJWT(verify, secret),
     authenticate: authenticate(users.get, secret, sign)
   }
 }
 Object.assign(module.exports,
   {
-    jwt,
     authenticate,
     mustHaveJWT
   }
