058ce65c0904c89e6e696869c6b9b64e1e3ddf18
MouseZero <rm@mousezero.com>
2017-04-05 12:49:21 -0700

    (feat) status-factory now has a working get function

--- src/persistence/status-factory.js
+++ src/persistence/status-factory.js
@@ -1,3 +1,18 @@
-module.exports = {userGoing}
+module.exports = (db) => {
+  return {userGoing, get}
 
-async function userGoing () {}
+  async function userGoing (userId, locationId) {
+    await db.query(`
+      INSERT INTO status
+      values
+      ($1, $2);
+      `, [locationId, [userId]])
+  }
+
+  async function get (id) {
+    const { rows: [result] } = await db.query(`
+      SELECT * FROM status WHERE id = $1
+      `, [id])
+    return result || null
+  }
+}
--- src/persistence/status-factory.spec.js
+++ src/persistence/status-factory.spec.js
@@ -1,10 +1,54 @@
-const { userGoing } = require('./status-factory')
+const statusFactory = require('./status-factory')
 const { expect } = require('chai')
+const { Pool } = require('pg')
+const { database: config } = require('../../config.json')
 
 describe('status-factory', () => {
+  const db = new Pool(config)
+  let status
+
+  beforeEach(() => {
+    status = statusFactory(db)
+    return db.query('begin')
+  })
+
+  afterEach(() => db.query('rollback'))
+
+  after(() => db.end)
+
   describe('addUser', () => {
     it('should be an async function', () => {
-      expect(userGoing).to.be.a('AsyncFunction')
+      expect(status.userGoing).to.be.a('AsyncFunction')
+    })
+    it('adds first user successfully', async () => {
+      await status.userGoing(7, 'bar-id')
+      const { rows: [result] } = await db.query(`
+        SELECT * FROM status WHERE id = $1;
+        `, ['bar-id'])
+      expect(result.id).to.equal('bar-id')
+      expect(result['users_going']).to.deep.equal([7])
+    })
+    it.skip('adds user to an already existing db entry', async () => {
+      await status.userGoing(7, 'bar-id')
+      await status.userGoing(10, 'bar-id')
+      await status.userGoing(20, 'bar-id')
+      const { rows: [result] } = await db.query(`
+        SELECT * FROM status WHERE id = $1;
+        `, ['bar-id'])
+      expect(false).to.equal(true)
+    })
+  })
+
+  describe('get', () => {
+    it('no item exists', async () => {
+      const result = await status.get('bar-id')
+      expect(result).to.equal(null)
+    })
+    it('gets existing items', async () => {
+      await db.query(`INSERT INTO status values ('bar-id', ARRAY[20, 9])`)
+      const {id, users_going} = await status.get('bar-id')
+      expect(id).to.equal('bar-id')
+      expect(users_going).eql([20, 9])
     })
   })
 })
