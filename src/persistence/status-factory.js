e15620ca39b6564d280fb1d375bb545095a6bd6c
MouseZero <rm@mousezero.com>
2017-04-05 13:21:42 -0700

    (feat) updates status in the db

--- src/persistence/status-factory.js
+++ src/persistence/status-factory.js
@@ -1,12 +1,23 @@
 module.exports = (db) => {
-  return {create, get}
+  return {create, get, update}
 
-  async function create (userId, locationId) {
+  async function create (locationId, userId) {
     await db.query(`
       INSERT INTO status
       values
       ($1, $2);
       `, [locationId, [userId]])
+  }
+
+  async function update (locationId, newUserId) {
+    const old = await get(locationId)
+    const newArray = [...old.users_going, newUserId]
+    const result = await db.query(`
+      UPDATE status
+      SET users_going = $1
+      WHERE id = $2
+      `, [newArray, locationId])
+    return result
   }
 
   async function get (id) {
--- src/persistence/status-factory.spec.js
+++ src/persistence/status-factory.spec.js
@@ -21,7 +21,7 @@ describe('status-factory', () => {
       expect(status.create).to.be.a('AsyncFunction')
     })
     it('can create', async () => {
-      await status.create(7, 'bar-id')
+      await status.create('bar-id', 7)
       const { rows: [result] } = await db.query(`
         SELECT * FROM status WHERE id = $1;
         `, ['bar-id'])
@@ -40,6 +40,15 @@ describe('status-factory', () => {
       const {id, users_going} = await status.get('bar-id')
       expect(id).to.equal('bar-id')
       expect(users_going).eql([20, 9])
+    })
+  })
+
+  describe('update', () => {
+    it('updates existing entry', async () => {
+      await db.query(`INSERT INTO status values ('some-id', ARRAY[2])`)
+      await status.update('some-id', 8)
+      const {rows: [result]} = await db.query(`SELECT * FROM status WHERE id = 'some-id'`)
+      expect(result.users_going).to.deep.equal([2, 8])
     })
   })
 })
