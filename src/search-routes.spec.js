44d8bd60c707acb7a62686dfc2022c34af96809c
MouseZero <rm@mousezero.com>
2017-04-15 21:39:30 -0700

    (test) Added more test for search-routes

--- src/search-routes.spec.js
+++ src/search-routes.spec.js
@@ -16,7 +16,16 @@ describe('search-routes', () => {
       const searchBars = () => {}
       return expect(requestBarData(searchBars)).to.be.rejectedWith(BadRequest)
     })
-    it('happy path')
+    it('throws error if searchBars fails', async () => {
+      const searchBars = () => new Promise.reject(new Error)
+      return expect(requestBarData(searchBars, 'irvine')).to.be.rejectedWith(Error)
+    })
+    it('passes data to searchBars', async () => {
+      let xArg
+      const searchBars = (x) => {xArg = x}
+      await requestBarData(searchBars, 'irvine')
+      expect(xArg).to.equal('irvine')
+    })
   })
 
   //TODO fix this test to only test search
@@ -27,48 +36,7 @@ describe('search-routes', () => {
     it('should return a function', () => {
       expect(search()).to.be.a('function')
     })
-    it('passes location to searchBars', async () => {
-      let passed
-      const spy = sinon.spy(async function (x) {
-        passed = x
-      })
-      const setup = (req, res, next) => {
-        req.query.location = 'irvine'
-        next()
-      }
-      await run(setup, search(spy))
-      expect(passed).to.equal('irvine')
-    })
-    it('returns json result if success', async () => {
-      const fakeResults = {
-        test: {
-          result: 1,
-          result2: 5
-        }
-      }
-      const spy = sinon.spy(() => Promise.resolve(fakeResults))
-      const setup = (req, res, next) => {
-        req.query.location = 'irvine'
-        res.json = x => { res.json = x }
-        next()
-      }
-      const [err, , {json}] = await run(setup, search(spy))
-      expect(err).to.equal(null)
-      expect(json.success).to.equal(true)
-      expect(json.result).to.deep.equal(fakeResults)
-    })
-    it('errers out if request for search fails', async () => {
-      const spy = sinon.spy(() => Promise.reject(new Error('error')))
-      const setup = (req, res, next) => next()
-      const [err] = await run(setup, search(spy))
-      expect(!!err).to.equal(true)
-    })
-    it('erros if it gets bad inputs', async () => {
-      await run(null, search(() => {}), (err) => {
-        expect(err).to.not.equal(null)
-        expect(err).to.be.instanceof(Error)
-      })
-    })
+    it('returns json from requestBarData promise')
   })
 
   describe('addNumberGoing', () => {
