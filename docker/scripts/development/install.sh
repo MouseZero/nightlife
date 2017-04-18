aa8df84c4e82e4588e5bbd3478d9f4e16c23480a
Russell Murray <rm@mousezero.com>
2017-04-18 15:27:32 -0700

    tmuxinator added

--- docker/scripts/development/install.sh
+++ docker/scripts/development/install.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 apt-get -qq update
 apt-get -y upgrade
-apt install -y tmux vim
+apt install -y tmux vim ruby
+export EDITOR='vim'
+export SHELL='/bin/bash'
+gem install tmuxinator
+cp -r docker/scripts/development/resources/.tmuxinator $HOME/
