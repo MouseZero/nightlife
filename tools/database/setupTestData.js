1aa6304e4afa4d1fab4c44c1ba781392e445afa4
MouseZero <rm@mousezero.com>
2017-04-29 21:10:21 -0700

    tool for creating "status" test data

--- tools/database/setupTestData.js
+++ tools/database/setupTestData.js
@@ -17,8 +17,33 @@ const createTestUser = () => {
   .then(x => console.log('Created Test User'))
   .catch(x => console.log(x))
 }
+const deleteTestStats = () => {
+  return db.query(`
+    DELETE FROM status WHERE id = 'bar1' or id = 'bar2';
+  `)
+  .then(x => console.log('deleted bar1'))
+}
+const createTestStats = () => {
+  return db.query(`
+  INSERT INTO
+  status
+  (id, users_going)
+  VALUES
+  ('bar1', '{5, 6, 9}');
+  INSERT INTO
+  status
+  (id, users_going)
+  VALUES
+  ('bar2', '{2, 6, 12}');
+  `)
+  .then(x => console.log('created bar2'))
+}
 
-Promise.all([deleteTestUser(), createTestUser()])
+Promise.all([
+  deleteTestUser(),
+  createTestUser(),
+  deleteTestStats(),
+  createTestStats()])
   .then(() => {
     console.log('All test data has been written')
     process.exit()
