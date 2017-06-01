a389deec162dd14f841085c24cfe7d842ab12b39
MouseZero <rm@mousezero.com>
2017-05-31 20:04:47 -0700

    enable CORS

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const statusDb = require('./src/persistence/status-factory')(pool)
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+  next()
+})
+
 app.use('/', routes(usersDb, statusDb))
 
 resetIntervals(statusDb)
