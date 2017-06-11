156897870d1191ac0deda482e589afcbada3e481
MouseZero <rm@mousezero.com>
2017-06-11 10:43:12 -0700

    removed clearIntervals

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require('./src/checkEnvVars')()
 const app = require('express')()
 const routes = require('./src/routes')
 const bodyParser = require('body-parser')
-const resetIntervals = require('./src/reset-intervals')
 const pg = require('pg')
 const config = require('./src/databaseCredentialsFromEnv')
 const pool = new pg.Pool(config)
@@ -20,8 +19,6 @@ app.use(cors())
 app.options('*', cors())
 
 app.use('/', routes(usersDb, statusDb))
-
-resetIntervals(statusDb)
 
 app.listen(PORT, function () {
   console.log(`App is listening on port ${PORT}`)
